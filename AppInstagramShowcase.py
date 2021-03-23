4a77dabdc2e700f3f0b9acaab8cfba474f8c6295
LanternD <yangdavid1028@gmail.com>
2021-03-22 20:46:18 -0400

    update the ins app routine

--- AppInstagramShowcase.py
+++ AppInstagramShowcase.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import time
+from datetime import datetime, timedelta
 from os.path import expanduser
 
 from PIL import Image, ImageDraw, ImageFont
@@ -17,6 +18,7 @@ class AppInstagramShowcase:
         self.photo_list = self.get_ins_photo_list()
 
     def get_ins_photo_list(self):
+        """Get all the files in the instagram folder. """
         photo_fn_list = get_file_list(self.ins_folder)
         # print(self.home_folder+self.ins_folder)
         # print(len(photo_list), photo_list[:10])
@@ -26,12 +28,24 @@ class AppInstagramShowcase:
         return photo_fn_list
 
     def show_one_ins_image(self, pic_path):
+        """Show one image by a given absolute path."""
         img = Image.open(pic_path)
         self.epd_drv.show_one_image(img)
 
-    def app_pipeline(self):
+    def app_pipeline(self, interval=timedelta(minutes=10)):
+        """The rountine to continuously display the photos."""
+
+        def is_working_hour(dt):
+            return (8, 59) < (dt.hour, dt.minute) < (23, 30)
+
+        previous_update = datetime.now() - timedelta(minutes=10, seconds=1)
         while True:
-            rand_path = random.choice(self.photo_list)
-            logging.info(f'Display: {rand_path}')
-            self.show_one_ins_image(self.ins_folder+rand_path)
-            time.sleep(10)
+            # Check every 15 seconds, update only during 9:00 - 23:30
+            time_now = datetime.now()
+            if time_now - previous_update > interval and is_working_hour(time_now):
+                rand_path = random.choice(self.photo_list)
+                time_str = time_now.strftime("%x %X")
+                logging.info(f"[{time_str}] {rand_path}")
+                self.show_one_ins_image(self.ins_folder + rand_path)
+                previous_update = time_now
+            time.sleep(15)
--- EPD7In8Driver.py
+++ EPD7In8Driver.py
@@ -21,30 +21,44 @@ class EPD7In8Driver:
         if virtual:
             from IT8951.display import VirtualEPDDisplay
 
-            self.disp = VirtualEPDDisplay(
-                dims=(self.width, self.height), rotate=rotate
-            )
+            self.disp = VirtualEPDDisplay(dims=(self.width, self.height), rotate=rotate)
         else:
-            logging.info('Initializing 7.8 inch e-paper display.')
+            logging.info("Initializing 7.8 inch e-paper display.")
             self.disp = AutoEPDDisplay(vcom=-1.4, rotate=rotate, spi_hz=EPD7IN8FREQ)
-            logging.info(f'VCOM set to {self.disp.epd.get_vcom()}')
-
+            logging.info(f"VCOM set to {self.disp.epd.get_vcom()}")
 
     def clear_frame(self):
-        '''Fill the frame with white dot
+        """Fill the frame with white dot
         Note: clear_frame is different from disp.clear(). The latter will write the data to the display hardware.
-        '''
+        """
         self.disp.frame_buf.paste(0xFF, box=(0, 0, self.width, self.height))
 
-
-    def show_one_image(self, img):
-        '''Input: img, an PIL object'''
+    def show_one_image(self, img_orig):
+        """Input: img, an PIL object"""
         self.clear_frame()
 
+        frame_width = 1760
+        frame_height = 1380
+        if img_orig.size[0]/img_orig.size[1] > frame_width/frame_height:
+            # left and right are the lower bound
+            ratio = frame_width / img_orig.size[0]
+            new_width = frame_width
+            new_height = int(img_orig.size[1] * ratio)
+            if new_height % 10 == 8:
+                new_height += 2
+        else:
+            # top/bottom are the lower bound
+            ratio = frame_height / img_orig.size[1]
+            new_height = frame_height
+            new_width = int(img_orig.size[0] * ratio)
+            if new_width % 10 == 8:
+                new_width += 2
+        img2 = img_orig.resize((new_width, new_height), Image.ANTIALIAS)
         dims = (self.width, self.height)
-        img.thumbnail(dims)
-        paste_coords = [dims[i] - img.size[i] for i in (0,1)]  # align image with bottom of display
-        self.disp.frame_buf.paste(img, paste_coords)
+        paste_coords = [
+            (dims[i] - img2.size[i])//2 for i in (0, 1)
+        ]  # align image with bottom of display
+        self.disp.frame_buf.paste(img2, paste_coords)
 
         self.disp.draw_full(constants.DisplayModes.GC16)
 
